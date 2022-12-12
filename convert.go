63b73b725d8dc838dbf79b341a09a7c6bb2cda76
andrijajelenkovic <andrija.jelenkovic@protonmail.com>
2022-12-12 21:46:18 +0100

    Command for converting a .mp4 video to HLS

---
+++ convert.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("conversion in progress")
+		}
+	}()
+	err := exec.Command(
+		"ffmpeg",
+		"-i",
+		"./video/filename.mp4",
+		"-codec:",
+		"copy",
+		"-start_number",
+		"0",
+		"-hls_time",
+		"10",
+		"-hls_list_size",
+		"0",
+		"-f",
+		"hls",
+		"./video/filename.m3u8",
+	).Run()
+
+	log.Println(err.Error())
+}

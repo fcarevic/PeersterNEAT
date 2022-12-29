32c827db00e810b92235463d413d1b4d22ae2e42
Natalija Mitic <naca@ntec.ch>
2022-12-29 17:08:33 +0100

    reduce crowds interface

--- peer/crowds.go
+++ peer/crowds.go
@@ -1,13 +1,7 @@
 package peer
 
-import "go.dedis.ch/cs438/transport"
-
 // Crowds defines the functions for crowds messaging
 type Crowds interface {
-	CrowdsInit(conf Configuration)
-	CrowdsDestroy()
-	StartCrowds(peers []string, isFileRequest bool, content, finalDst string) ([]byte, error)
-	SendCrowdsMessage(embeddedMsg *transport.Message, recipients []string) error
-	CreateCrowdsMessagingRequest(dst, content string) (transport.Message, error)
-	CreateCrowdsDownloadRequest(reqID, content string) (transport.Message, error)
+	CrowdsSend(peers []string, body, to string) error
+	CrowdsDownload(peers []string, filename string) ([]byte, error)
 }
